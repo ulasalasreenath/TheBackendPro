c36134783fd0610112f4e313d0c8e19891772f6c
Sreenath Ulasala <ulasala.sreenath@sap.com>
2022-06-05 23:12:39 +0530

    How to Assert Two Lists for Equality Ignoring Order in Java - Using AssertJ

---
+++ src/test/java/ListTestWithAssertJ.java
@@ -0,0 +1,16 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ListTestWithAssertJ {
+
+    @Test
+    public void compareListIgnoreOrder() {
+        List list1 = List.of("A", "B", "C");
+        List list2 = List.of("B", "C", "A", "A");
+
+        Assertions.assertThat(list1).hasSameElementsAs(list2);
+    }
+
+}
